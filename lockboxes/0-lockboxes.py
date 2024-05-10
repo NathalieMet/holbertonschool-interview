d35012a34619b9a8634f42f54a8199d5947e8d98
nathalie <nathalie.metreau@hotmail.fr>
2024-05-10 19:58:35 +0200

    correct list out of range problem

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -23,10 +23,10 @@ def canUnlockAll(boxes):
     while index < len(already_open):
         key = already_open[index]
         for i in boxes[key]:
-            if i <= len(boxes) and i not in already_open:
+            if i < len(boxes) and i not in already_open:
                 already_open.append(i)
-                # print(already_open, len(already_open), len(boxes))
         index += 1
+        
     return len(already_open) == len(boxes)
 
 
