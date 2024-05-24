dde0b41dae7c5f98ead1ad41a459abcb40ab7e98
nathalie <nathalie.metreau@hotmail.fr>
2024-05-24 11:50:30 +0200

    add comment for return on task 0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -6,6 +6,8 @@
  *
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
+ *
+ * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
