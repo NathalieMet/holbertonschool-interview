cd8310cffebac5097242992076f47363b0885028
nathalie <nathalie.metreau@hotmail.fr>
2024-07-29 15:45:03 +0200

    correct pycodestyle and remove one line on error cases

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def print_solution(board):
     """ Print the board solution """
     solution = []
@@ -10,6 +11,7 @@ def print_solution(board):
             if board[row][col] == 1:
                 solution.append([row, col])
     print(solution)
+
 
 def is_safe(board, row, col, N):
     """ Check if it's safe to place a queen at board[row][col] """
@@ -30,6 +32,7 @@ def is_safe(board, row, col, N):
 
     return True
 
+
 def solve_nqueens_util(board, col, N):
     """ Solve the N Queens problem using backtracking """
     if col >= N:
@@ -45,29 +48,31 @@ def solve_nqueens_util(board, col, N):
 
     return res
 
+
 def solve_nqueens(N):
     """ Initialize the board and start solving """
     board = [[0 for _ in range(N)] for _ in range(N)]
     if not solve_nqueens_util(board, 0, N):
         print("Solution does not exist")
 
+
 def nqueens():
     if len(sys.argv) != 2:
-        print("Usage: nqueens N\n")
+        print("Usage: nqueens N")
         exit(1)
 
     try:
         N = int(sys.argv[1])
     except ValueError:
-        print("N must be a number\n")
+        print("N must be a number")
         exit(1)
 
     if N < 4:
-        print("N must be at least 4\n")
+        print("N must be at least 4")
         exit(1)
 
     solve_nqueens(N)
 
+
 if __name__ == "__main__":
     nqueens()
-
