b8807e073c1c1eb491dbc826bf3716f2b7dddc96
nathalie <nathalie.metreau@hotmail.fr>
2024-07-29 15:41:21 +0200

    add task 0 nqueens

---
+++ nqueens/0-nqueens.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import sys
+
+def print_solution(board):
+    """ Print the board solution """
+    solution = []
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == 1:
+                solution.append([row, col])
+    print(solution)
+
+def is_safe(board, row, col, N):
+    """ Check if it's safe to place a queen at board[row][col] """
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens_util(board, col, N):
+    """ Solve the N Queens problem using backtracking """
+    if col >= N:
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, N) or res
+            board[i][col] = 0  # Backtrack
+
+    return res
+
+def solve_nqueens(N):
+    """ Initialize the board and start solving """
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    if not solve_nqueens_util(board, 0, N):
+        print("Solution does not exist")
+
+def nqueens():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N\n")
+        exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number\n")
+        exit(1)
+
+    if N < 4:
+        print("N must be at least 4\n")
+        exit(1)
+
+    solve_nqueens(N)
+
+if __name__ == "__main__":
+    nqueens()
+
