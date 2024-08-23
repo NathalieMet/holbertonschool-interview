5cb9ef1689fa0dcbacc40621396aca67b04c0c7e
nathalie <nathalie.metreau@hotmail.fr>
2024-08-23 13:35:54 +0200

    correct semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,38 +4,38 @@ const util = require('util');
 
 const requestPromise = util.promisify(request);
 
-async function getCharacterName(url) {
-    try {
-        const response = await requestPromise(url);
-        const characterData = JSON.parse(response.body);
-        return characterData.name;
-    } catch (error) {
-        console.error('Error fetching character:', error.message);
-    }
+async function getCharacterName (url) {
+  try {
+    const response = await requestPromise(url);
+    const characterData = JSON.parse(response.body);
+    return characterData.name;
+  } catch (error) {
+    console.error('Error fetching character:', error.message);
+  }
 }
 
-async function printMovieCharacters(movieId) {
-    if (!movieId) {
-        console.error('Usage: node script.js <movie>');
-        process.exit(1);
+async function printMovieCharacters (movieId) {
+  if (!movieId) {
+    console.error('Usage: node script.js <movie>');
+    process.exit(1);
+  }
+
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  try {
+    const response = await requestPromise(url);
+    const movieData = JSON.parse(response.body);
+    const characters = movieData.characters;
+
+    for (const characterUrl of characters) {
+      const name = await getCharacterName(characterUrl);
+      if (name) {
+        console.log(name);
+      }
     }
-
-    const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-    try {
-        const response = await requestPromise(url);
-        const movieData = JSON.parse(response.body);
-        const characters = movieData.characters;
-
-        for (const characterUrl of characters) {
-            const name = await getCharacterName(characterUrl);
-            if (name) {
-                console.log(name);
-            }
-        }
-    } catch (error) {
-        console.error('Error fetching movie data:', error.message);
-    }
+  } catch (error) {
+    console.error('Error fetching movie data:', error.message);
+  }
 }
 
 const movieId = process.argv[2];
