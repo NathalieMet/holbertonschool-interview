44128c234da09b1680463cd7975615dcb63e6f6a
nathalie <nathalie.metreau@hotmail.fr>
2024-08-23 12:55:54 +0200

    try correction on t0

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,42 +3,42 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-
 if (!movieId) {
-	console.error('Usage: node script.js <movie>');
-	process.exit(1);
+    console.error('Usage: node script.js <movie>');
+    process.exit(1);
 }
 
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error.message);
-    return;
-  }
+    if (error) {
+        console.error('Error:', error.message);
+        return;
+    }
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve data. Status code:', response.statusCode);
-    return;
-  }
+    if (response.statusCode !== 200) {
+        console.error('Failed to retrieve data. Status code:', response.statusCode);
+        return;
+    }
 
-  try {
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-	characters.forEach((characterUrl) => {
-		request(characterUrl, (charError, charResponse, charBody) => {
-		  if (charError) {
-			console.error('Error:', charError.message);
-			return;
-		  }
+    try {
+        const movieData = JSON.parse(body);
+        const characters = movieData.characters;
 
-		  if (charResponse.statusCode === 200) {
-			const characterData = JSON.parse(charBody);
-			console.log(characterData.name);
-		  }
-		});
-	  });
-	} catch (parseError) {
-	  console.error('Error parsing JSON:', parseError.message);
-	}
-  });
+        characters.forEach((characterUrl) => {
+            request(characterUrl, (charError, charResponse, charBody) => {
+                if (charError) {
+                    console.error('Error:', charError.message);
+                    return;
+                }
+
+                if (charResponse.statusCode === 200) {
+                    const characterData = JSON.parse(charBody);
+                    console.log(characterData.name);
+                }
+            });
+        });
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError.message);
+    }
+});
