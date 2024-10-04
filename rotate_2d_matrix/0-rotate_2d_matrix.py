f7fb666ea96f700d23715c0f4a04c6ca8385d817
nathalie <nathalie.metreau@hotmail.fr>
2024-10-04 21:36:50 +0200

    correct doc

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+rotate matrix 90 degrees clockwise.
+"""
 
 def rotate_2d_matrix(matrix):
+    """
+rotate matrix 90 degrees clockwise.
+"""
     rotated_matrix = list(zip(*matrix[::-1]))
 
     for i in range(len(matrix)):
