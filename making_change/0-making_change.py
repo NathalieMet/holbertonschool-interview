eeba3c5322774e1cc28c86075dbe49816ba25220
nathalie <nathalie.metreau@hotmail.fr>
2024-10-21 18:13:33 +0200

    TRY CORRECTION

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -7,30 +7,36 @@ amount total
 
 def makeChange(coins, total):
     """
-Function that determines the fewest number of coins needed to meet a given
-amount total
-"""
+    Function that determines the fewest number of coins needed to meet a given
+    amount total
+    """
     if total <= 0:
         return 0
 
     coins.sort(reverse=True)
-
-
-    count = 1
+    count = 0
 
     for first in coins:
+        if first == total:
+            count += 1
+            return count
+
         for second in coins:
             if first + second == total:
-                count += 1
-
+                count += 2
                 return count
-            if first == total:
 
-                return count
             if first + second > total:
                 continue
-            while first + second <= total:
-                first += second
+
+            current_sum = first
+            count = 1
+
+            while current_sum + second <= total:
+                current_sum += second
                 count += 1
 
-        return -1
+            if current_sum == total:
+                return count
+
+    return -1
