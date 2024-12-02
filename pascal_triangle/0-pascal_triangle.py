48c5a985157279b0dc4fe1aa973147a5b6764757
nathalie <nathalie.metreau@hotmail.fr>
2024-12-02 23:18:28 +0100

    correct documentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Create a function def pascal_triangle(n): that returns a list of lists of
+integers representing the Pascal’s triangle of n:
+
+Returns an empty list if n <= 0
+You can assume n will be always an integer"""
+
 
 def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle up to level n."""
