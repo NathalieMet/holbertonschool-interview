d80d7820e1ccc238366201105205b3c6d37ddec3
nathalie <nathalie.metreau@hotmail.fr>
2024-12-29 22:40:11 +0100

    delete from for the chcker

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Maria and Ben are playing a game. Given a set of consecutive integers starting
-from 1 up to and including n, they take turns choosing a prime number from the
-set and removing that number and its multiples from the set. The player that
+frm 1 up to and including n, they take turns choosing a prime number frm the
+set and removing that number and its multiples frm the set. The player that
 cannot make a move loses the game.
 
 They play x rounds of the game, where n may be different for each round.
